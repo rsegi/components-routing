22914fa5913eaffca1d37a6210ff13250d3fa0db
Raul Segui <r.segui.t@gmail.com>
2020-05-08 22:53:02 +0200

    save process

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,13 +3,49 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    {
+      name: 'James',
+      age: 24,
+      job: 'Designer',
+      employed: true,
+    },
+    {
+      name: 'Yu',
+      age: 24,
+      job: 'Engineer',
+      employed: false,
+    },
+    {
+      name: 'Gylfoyle',
+      age: 31,
+      job: 'Engineer',
+      employed: true,
+    },
+  ];
+  headers = [
+    {
+      key: 'name',
+      label: 'Name',
+    },
+    {
+      key: 'age',
+      label: 'Age',
+    },
+    {
+      key: 'job',
+      label: 'Job',
+    },
+    {
+      key: 'employed',
+      label: 'Has a Job?',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
